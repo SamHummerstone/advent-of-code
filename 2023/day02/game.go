b68de022f4bec03cff041af9fd8ccfb5be2803cd
SamHummerstone <sjhummerstone@gmail.com>
2024-11-26 21:40:20 +0000

    Part 2 solution ad test

--- 2023/day02/game.go
+++ 2023/day02/game.go
@@ -7,50 +7,45 @@ import (
 	"strings"
 )
 
-type Game struct {
-	Id    int
-	Red   int
-	Green int
-	Blue  int
+type Colour struct {
+	Max int
+	// Min int // Not needed, I was overcomplicating things
 }
 
-type GameDice struct {
+type Game struct {
+	Id    int
+	Red   Colour
+	Green Colour
+	Blue  Colour
+}
+
+type GameLimits struct {
 	Red   int
 	Green int
 	Blue  int
 }
 
 func New(s string) *Game {
-	var red, green, blue int = 0, 0, 0
-	idStr, rem, _ := strings.Cut(s, ":")
-	id, err := strconv.Atoi(strings.TrimPrefix(idStr, "Game "))
-	if err != nil {
-		fmt.Println("Could not convert ID to int:", err)
-		os.Exit(1)
-	}
+	var red, green, blue Colour
+	id, rem := IDFromString(s)
 
 	// Split into rounds
 	for _, r := range strings.Split(rem, ";") {
 		// Split into colours
 		for _, c := range strings.Split(r, ",") {
-			colourCount := strings.Split(strings.Trim(c, " "), " ")
-			count, err := strconv.Atoi(colourCount[0])
-			if err != nil {
-				fmt.Printf("Cannot convert str to int: %v", err)
-				os.Exit(1)
-			}
-			switch colour := colourCount[1]; colour {
+			count, colour := GetColourAndCount(c)
+			switch colour {
 			case "red":
-				if count > red {
-					red = count
+				if count > red.Max {
+					red.Max = count
 				}
 			case "blue":
-				if count > blue {
-					blue = count
+				if count > blue.Max {
+					blue.Max = count
 				}
 			case "green":
-				if count > green {
-					green = count
+				if count > green.Max {
+					green.Max = count
 				}
 			}
 		}
@@ -63,13 +58,13 @@ func New(s string) *Game {
 	}
 }
 
-func (g Game) CheckGame(check GameDice) bool {
+func (g Game) CheckGame(check GameLimits) bool {
 	switch {
-	case g.Red > check.Red:
+	case g.Red.Max > check.Red:
 		return false
-	case g.Blue > check.Blue:
+	case g.Blue.Max > check.Blue:
 		return false
-	case g.Green > check.Green:
+	case g.Green.Max > check.Green:
 		return false
 	default:
 		return true
@@ -77,5 +72,34 @@ func (g Game) CheckGame(check GameDice) bool {
 }
 
 func (g Game) Power() int {
-	return g.Red * g.Green * g.Blue
+	return g.Red.Max * g.Green.Max * g.Blue.Max
+}
+
+// Get game ID from the beginning of the string and
+// return the ID and the remainder of the string
+func IDFromString(s string) (int, string) {
+	before, after, found := strings.Cut(s, ": ")
+	if !found {
+		fmt.Println("String doesn't include ': ' character")
+		return 0, ""
+	}
+	id, err := strconv.Atoi(strings.TrimPrefix(before, "Game "))
+	if err != nil {
+		fmt.Println("IDFromString:", err)
+	}
+
+	return id, after
+}
+
+// Get colour and count from string
+func GetColourAndCount(s string) (int, string) {
+	colourCount := strings.Split(strings.Trim(s, " "), " ")
+	count, err := strconv.Atoi(colourCount[0])
+	if err != nil {
+		fmt.Printf("Cannot convert str to int: %v", err)
+		os.Exit(1)
+	}
+	colour := colourCount[1]
+
+	return count, colour
 }
--- 2023/day02/main.go
+++ 2023/day02/main.go
@@ -22,19 +22,21 @@ func main() {
 	lines := strings.Split(string(input), "\n")
 
 	games := LinestoGames(lines)
-	gameDice := GameDice{
+	gameLimits := GameLimits{
 		Red:   12,
 		Green: 13,
 		Blue:  14,
 	}
 
-	solution1 := Part1(games, gameDice)
+	solution1 := Part1(games, gameLimits)
+	solution2 := Part2(games)
 
 	fmt.Println(solution1)
+	fmt.Println(solution2)
 
 }
 
-func Part1(games []*Game, gd GameDice) int {
+func Part1(games []*Game, gd GameLimits) int {
 	var idSum int = 0
 	for _, g := range games {
 		if g.CheckGame(gd) {
@@ -43,6 +45,14 @@ func Part1(games []*Game, gd GameDice) int {
 	}
 
 	return idSum
+}
+
+func Part2(games []*Game) int {
+	var powerSum int = 0
+	for _, g := range games {
+		powerSum += g.Power()
+	}
+	return powerSum
 }
 
 func LinestoGames(lines []string) []*Game {
