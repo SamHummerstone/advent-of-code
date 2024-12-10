07a161279586f20278108c82a7daa810e08cd87e
SamHummerstone <sjhummerstone@gmail.com>
2024-12-10 21:51:26 +0000

    Add search functions

--- 2024/day04/main.go
+++ 2024/day04/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"embed"
 	"fmt"
+	"strings"
 )
+
+type Board [][]string
 
 //go:embed inputFile.txt
 var inputFile embed.FS
@@ -13,14 +16,62 @@ func main() {
 	if err != nil {
 		panic("Could not read input file")
 	}
-	fmt.Println(Part1(inputFile))
+	fmt.Println(Part1(string(inputFile)))
 	fmt.Println(Part2(inputFile))
 }
 
-func Part1(input []byte) int {
+func Part1(input string) int {
+	match := []string{"X", "M", "A", "S"}
+	board := CreateBoard(input)
+
+	TraverseBoard(board, match, func() {
+		fmt.Println("tada")
+	})
 	return 0
 }
 
 func Part2(input []byte) int {
 	return 0
+}
+
+func CreateBoard(input string) Board {
+	rows := strings.Split(string(input), "\n")
+	board := make(Board, len(rows))
+
+	for y, row := range rows {
+		board[y] = make([]string, len(row))
+		for x, r := range row {
+			board[y][x] = string(r)
+		}
+	}
+	return board
+}
+
+func TraverseBoard(board Board, match []string, f func()) {
+	for y, row := range board {
+		for x, col := range row {
+			if col == match[0] {
+				fmt.Printf("Found an %v at pos (%v,%v)", col, x, y)
+				f()
+			}
+		}
+	}
+}
+
+func SearchX(board [][]string, match []string, startX, startY int, directionX int) bool {
+	for i := 0; i < len(match); i++ {
+		if board[startY][startX+(i*directionX)] != match[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func SearchY(board [][]string, match []string, startX, startY int, directionY int) bool {
+	for i := 0; i < len(match); i++ {
+		if board[startY+(i*directionY)][startX] != match[i] {
+			return false
+		}
+	}
+	return true
 }
--- 2024/day04/main_test.go
+++ 2024/day04/main_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-var testInput = []byte(`MMMSXXMASM
+var testInput = `MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -11,7 +14,7 @@ XXAMMXXAMA
 SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
-MXMXAXMASX`)
+MXMXAXMASX`
 
 func Test_Part1(t *testing.T) {
 	want := 18
@@ -21,4 +24,94 @@ func Test_Part1(t *testing.T) {
 			t.Errorf("Got %v:\nWant: %v", got, want)
 		}
 	})
+}
+
+func Test_SearchX(t *testing.T) {
+	match := []string{"X", "M", "A", "S"}
+	tcs := []struct {
+		Input     []string
+		Direction int
+		Expect    bool
+	}{
+		{
+			Input:     match,
+			Direction: 1,
+			Expect:    true,
+		},
+		{
+			Input:     match,
+			Direction: -1,
+			Expect:    false,
+		},
+		{
+			Input:     []string{"S", "A", "M", "X"},
+			Direction: 1,
+			Expect:    false,
+		},
+		{
+			Input:     []string{"S", "A", "M", "X"},
+			Direction: -1,
+			Expect:    true,
+		},
+	}
+
+	for i, tc := range tcs {
+		t.Run(fmt.Sprintf("Test SearchX - case %v", i), func(t *testing.T) {
+			want := tc.Expect
+			var got bool
+			if tc.Direction < 0 {
+				got = SearchX([][]string{tc.Input}, match, len(tc.Input)-1, 0, tc.Direction)
+			} else {
+				got = SearchX([][]string{tc.Input}, match, 0, 0, tc.Direction)
+			}
+			if want != got {
+				t.Errorf("Want: %v\nGot: %v", want, got)
+			}
+		})
+	}
+}
+
+func Test_SearchY(t *testing.T) {
+	match := []string{"X", "M", "A", "S"}
+	tcs := []struct {
+		Input     []string
+		Direction int
+		Expect    bool
+	}{
+		{
+			Input:     match,
+			Direction: 1,
+			Expect:    true,
+		},
+		{
+			Input:     match,
+			Direction: -1,
+			Expect:    false,
+		},
+		{
+			Input:     []string{"S", "A", "M", "X"},
+			Direction: 1,
+			Expect:    false,
+		},
+		{
+			Input:     []string{"S", "A", "M", "X"},
+			Direction: -1,
+			Expect:    true,
+		},
+	}
+
+	for i, tc := range tcs {
+		t.Run(fmt.Sprintf("Test SearchY - case %v", i), func(t *testing.T) {
+			want := tc.Expect
+			var got bool
+			if tc.Direction < 0 {
+				got = SearchY([][]string{tc.Input}, match, 0, len(tc.Input)-1, tc.Direction)
+			} else {
+				got = SearchY([][]string{tc.Input}, match, 0, 0, tc.Direction)
+			}
+			if want != got {
+				t.Errorf("Want: %v\nGot: %v", want, got)
+			}
+		})
+	}
 }
